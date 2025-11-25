79e1761b1e32474624bfd5ef174bcab6cda07438
Marijn <m.r.van.dijk.3@student.rug.nl>
2025-11-25 14:57:08 +0000

    Added Fluid.jl file. Started migrating Fluid.py to Fluid.jl

---
+++ src/Fluid.jl
@@ -0,0 +1,26 @@
+
+
+module Fluid
+
+    # Constants
+    AU = 1.495978707e11  # m
+    G  = 6.6743e-11  # m^3 kg^-1 s^-2
+
+    # Degree Love number
+    n = 2
+    m = 2
+    k_min = -30
+    k_max = 40
+    k_range = np.arange(k_min, k_max + 1)
+
+    # Get fluid tidal heating
+
+    # Identiy liquid region
+
+    # Calculate eccentric anomaly
+
+    # Hansen coefficients
+
+    # Calculate Love number
+
+end
