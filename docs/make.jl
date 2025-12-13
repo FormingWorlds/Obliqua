4537c320de4586e124c58f3a858db41074331e98
Marijn <m.r.van.dijk.3@student.rug.nl>
2025-12-13 21:53:05 +0000

    Updated docs make.jl file

--- docs/make.jl
+++ docs/make.jl
@@ -1,27 +1,33 @@
 using Documenter
 using Love
 
-format = Documenter.HTML(edit_link = "main",
-                         prettyurls = get(ENV, "CI", nothing) == "true",
+# HTML format configuration
+format = Documenter.HTML(
+    edit_link = "main",  # branch name for "Edit on GitHub"
+    prettyurls = get(ENV, "CI", nothing) == "true",
 )
 
+# Build the docs
 makedocs(
     sitename="Love.jl",
     format=format,
+    modules = [Love],
     pages = [
         "Home" => "index.md",
-        "model/index.md",
-        "setup.md" ,
-        "usage.md",
-        "examples/index.md",
-        "troubleshooting.md",
-        "Development" => "manual/index.md",
+        "Installation" => "install.md",
+        "Compass" => "compass.md",
+        "Tutorials" => "tutorials/index.md",
+        "How-to guides" => "how-to-guides/index.md",
+        "Reference" => "reference/index.md",
+        "Explanation" => "explanation/index.md",
+        "Troubleshooting" => "troubleshooting.md",
+        "Development" => "development.md",
         "Related codes" => "ecosystem.md"
     ]
 )
 
 deploydocs(
-    repo = "github.com/FormingWorlds/Love.jl.git",
+    repo = "https://github.com/FormingWorlds/Love.jl.git",
     push_preview=true
 )
 
